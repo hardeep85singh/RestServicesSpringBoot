3b45830d4a7bfc840eae1d3899d7cfb5b8eaca7e
hardeep85singh <hardeep85singh@gmail.com>
2021-03-12 16:42:05 +0530

    Rest services with Spring Boot

---
+++ src/main/java/com/example/RestServiceSpringBoot/RestServiceSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/RestServiceSpringBoot/RestServiceSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RestServiceSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestServiceSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
